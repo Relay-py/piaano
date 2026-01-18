c8f5cbe94b4c2a78de729832c2418830eb3b007e
Justin <thisisjustinmao@gmail.com>
2026-01-17 20:10:39 -0500

    Connect instrument top and front, not fully working

--- main.py
+++ main.py
@@ -231,14 +231,24 @@ def main():
                 # Draw hand points
                 front_frame = draw_functions.draw_hand_points(front_frame, front_hand_keypoints)
 
-                # Filter for pressed fingers
-                pressed_fingers = instrument_front.get_pressed_fingers(front_hand_keypoints)
 
                 # convert and draw frame in pygame4
                 draw_functions.draw_frame(screen=pygame_screen, frame=front_frame, 
                                           x = display_width, y = display_height,
                                           width=display_width, height=display_height)
-        
+                
+            if top_cap.isOpened() and front_cap.isOpened() and len(top_hand_keypoints) > 0 and len(front_hand_keypoints) > 0:
+                # Filter for pressed fingers
+                pressed_fingers = instrument_front.get_pressed_fingers(front_hand_keypoints)
+
+                # Get playing notes
+                playing_notes = instrument_top.get_notes(pressed_fingers, white_key_tops, white_key_bases, black_key_tops, black_key_bases)
+
+                print(pressed_fingers)
+                print(corner_positions)
+                print("Playing notes!", playing_notes)
+                print("----------------")
+
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
